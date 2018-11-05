112343374f8287538d3a892cfde72de4fca4a06b
Anna Rankin <anna@annarankin.com>
2018-11-04 23:59:48 -0500

    looping pet query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ActionController::API
   def show
-    user = User.includes(:pets).find(params[:id])
+    user = User
+      .includes(:pets)
+      .find(params[:id])
+
+    upcoming_appointments = user
+      .appointments
+      .where(
+        'pet_id IN (?) AND date >= ?',
+        user.pets.pluck(:id),
+        Time.current
+      )
+      .order(date: :asc)
 
     render json: {
       user: {
@@ -8,9 +19,16 @@ class UsersController < ActionController::API
         name: "#{user.first_name} #{user.last_name}"
       },
       pets: user.pets.map do |pet|
+        next_appointment_date = user
+          .appointments
+          .order(date: :asc)
+          .find_by('date >= ?', Time.current)
+          &.date
+
         {
           id: pet.id,
-          name: pet.name
+          name: pet.name,
+          next_appointment_date: next_appointment_date
         }
       end
     }
